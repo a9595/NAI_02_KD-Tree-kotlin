a7ccc34509cc1d2976c3e0d6c5bd07faa1fac5fe
Andrii Kovalchuk <tieorange@gmail.com>
2017-03-18 20:17:42 +0100

    renaming

--- src/algor/MainKDTNearest.java
+++ src/algor/MainKDTNearest.java
@@ -11,31 +11,33 @@ import java.io.InputStreamReader;
 public class MainKDTNearest {
 
     public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new FileReader("input.txt"));
+//        BufferedReader in = new BufferedReader(new FileReader("input.txt"));
         int numpoints = 5;
 
-        KDTree kdt = new KDTree(numpoints);
+        KDTree kdTree = new KDTree(numpoints);
         double x[] = new double[2];
+
+        double[][] matrxi = new double[2][2]; // MATRIX
 
         x[0] = 2.1;
         x[1] = 4.3;
-        kdt.add(x);
+        kdTree.add(x);
 
         x[0] = 3.3;
         x[1] = 1.5;
-        kdt.add(x);
+        kdTree.add(x);
 
         x[0] = 4.7;
         x[1] = 11.1;
-        kdt.add(x);
+        kdTree.add(x);
 
         x[0] = 5.0;
         x[1] = 12.3;
-        kdt.add(x);
+        kdTree.add(x);
 
         x[0] = 5.1;
         x[1] = 1.2;
-        kdt.add(x);
+        kdTree.add(x);
 
         System.out
                 .println("Enter the co-ordinates of the point: (one after the other)");
@@ -44,10 +46,10 @@ public class MainKDTNearest {
         double sx = Double.parseDouble(br.readLine());
         double sy = Double.parseDouble(br.readLine());
 
-        double s[] = {sx, sy};
-        KDNode kdn = kdt.find_nearest(s);
+        double userInputArray[] = {sx, sy};
+        KDNode kdNode = kdTree.find_nearest(userInputArray);
         System.out.println("The nearest neighbor is: ");
-        System.out.println("(" + kdn.x[0] + " , " + kdn.x[1] + ")");
-        in.close();
+        System.out.println("(" + kdNode.x[0] + " , " + kdNode.x[1] + ")");
+//        in.close();
     }
 }
